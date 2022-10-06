a77a3dde35cd9fc5be997afe13eee955de9b1faf
nc163 <kou1992@gmail.com>
2022-10-06 15:23:48 +0000

    fix:

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -12,21 +12,46 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (method) {
     case 'POST':
       const { message, signature } = req.body
-      const siweMessage = new SiweMessage(message)
+      let sendValue = false
       try {
-        await siweMessage.validate(signature)
+        /*  eip-4361 message
+         *  https://eips.ethereum.org/EIPS/eip-4361
+         *  {
+         *    domain: 'localhost:3000',
+         *    address: '0x0000000000000000000000000000000000000000',
+         *    statement: 'Sign in with Ethereum to the app.',
+         *    uri: 'http://localhost:3000',
+         *    version: '1',
+         *    nonce: 'XXXXXXXXXXXXXXXXX',
+         *    issuedAt: '2000-01-01T00:00:00.000Z',
+         *    expirationTime: undefined,
+         *    notBefore: undefined,
+         *    requestId: undefined,
+         *    chainId: 1,
+         *    resources: undefined
+         *  }
+         */
+        const siweMessage = new SiweMessage(message)
 
-        await prisma.account.upsert({
-          where: { ethAddr: siweMessage.address },
-          update: { ethAddr: siweMessage.address, nance: siweMessage.nonce },
-          create: { ethAddr: siweMessage.address, nance: siweMessage.nonce },
+        let fields = await siweMessage.validate(signature)
+        if (fields.nonce !== req.session.nonce) return res.status(422).json({ message: 'Invalid nonce.' })
+
+        let address = siweMessage.address
+
+        // TODO: ENS-Name-Address to Wallet-Address
+        // https://docs.ens.domains/dapp-developer-guide/working-with-ens
+
+        const account = await prisma.account.upsert({
+          where: { ethAddr: address },
+          update: { ethAddr: address, nance: siweMessage.nonce },
+          create: { ethAddr: address, nance: siweMessage.nonce },
         })
 
-        res.send(true)
+        sendValue = true
       } catch (e) {
-        console.log(e)
-        res.send(false)
+        console.error(e)
       }
+      res.send(sendValue)
       break
   }
 }
